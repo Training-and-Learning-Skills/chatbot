f2ba9ec61065663e997d48e938c1a8e8b3b18bf5
Luciano Santos <luciano_z7@hotmail.com>
2018-09-14 00:03:47 -0300

    Created message class and converse method.

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { ApiAiClient } from 'api-ai-javascript';
+
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs';
+
+export class Message{
+  constructor(public content: string, public sentBy: string){}
+}
+
+
+@Injectable()
 export class ChatService {
 
+  readonly token = environment.dialogflow.angularBot;
+  readonly client = new ApiAiClient({ accessToken: this.token });
+
+  conversation = new BehaviorSubject<Message[]>([]);
+
   constructor() { }
+
+  //sends and receives messages via DialogFlow
+  converse(msg: string){
+    const userMessage = new Message(msg, 'user');
+    this.update(userMessage);
+
+    return this.client.textRequest(msg)
+      .then(res=>{
+        const speech = res.result.fulfillment.speech;
+        const botMessage = new Message(speech, 'bot');
+        this.update(botMessage);
+      });
+  }
+
+  // Add message to source
+  update(msg: Message){
+    this.conversation.next([msg]);
+  }
 }
